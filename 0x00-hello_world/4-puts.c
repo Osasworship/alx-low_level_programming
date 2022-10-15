45bc4852ccdce13bb83efee9ef5d8b48583dc291
Osasworship <amadasunosarugue010@gmail.com>
2022-10-15 05:22:28 -0700

    Creating a C program that prints using function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - make use of the function puts
+ *
+ * Return: Zero if there is no errors otherwise, return a non-zero value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
