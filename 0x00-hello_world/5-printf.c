85c993ccb07ada72636e9c033d27258a251f16cc
Osasworship <amadasunosarugue010@gmail.com>
2022-10-15 05:43:24 -0700

    How to print C program using the function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Make use of the function printf
+ *
+ * Return: Zero if there is no error otherwise, return a non-zero value
+ */
+int main(void)
+{
+	printf("with the proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
