b49b7111d35ed13a437a67d7139bb3f56cd3dcab
Osasworship <amadasunosarugue010@gmail.com>
2022-10-15 10:43:19 -0700

    How to prints the sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(a));
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(b));
-	printf("The size of a long int is: %lu.\n", (unsigned long)sizeof(c));
-	printf("The size of a long long int is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of a float is: %lu.\n", (unsigned long)sizeof(e));
+	printf("The size of a char is: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("The size of an int is: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("The size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("The size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(e));
 
 	return (0);
 }
