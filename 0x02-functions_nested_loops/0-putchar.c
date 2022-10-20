3fbdeef93851d68b9961b40dd564c48b306f57b5
Osasworship <amadasunosarugue010@gmail.com>
2022-10-20 14:56:48 -0700

    How to write a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Print _putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int letter;
+
+	letter = '_';
+	putchar(letter);
+	letter = 'p';
+	putchar(letter);
+	letter = 'u';
+	putchar(letter);
+	letter = 't';
+	putchar(letter);
+	letter = 'c';
+	putchar(letter);
+	letter = 'h';
+	putchar(letter);
+	letter = 'a';
+	putchar(letter);
+	letter = 'r';
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}
