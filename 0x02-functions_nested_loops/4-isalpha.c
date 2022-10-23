24708e278ac8650b1e28bcb29c7eed85939dc82b
Osasworship <amadasunosarugue010@gmail.com>
2022-10-23 11:19:35 -0700

    How to print alphabet in lowercase and uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - check for alphabetic char
+ * @c: is the char to be checked
+ *
+ * Return: 1 if c is a letter lowercase or uppercase otherwise 0
+ */
+int _isalpha(int c)
+{
+	if i((c = 'a' && c <= 'z') || (c = 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
