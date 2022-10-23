748fd7eb00a896e4049ad279637cd63517cd3ed7
Osasworship <amadasunosarugue010@gmail.com>
2022-10-23 08:45:51 -0700

    How to print the alphabet in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,12 @@
 /**
  * print_alphabet - print the alphabet in lower case
  */
+
 void print_alphabet(void);
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-
 	_putchar('\n');
 }
