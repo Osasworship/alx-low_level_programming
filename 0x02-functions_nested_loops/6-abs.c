4ce2c93e961e3968b61ef8afe5bb757961e5ed82
Osasworship <amadasunosarugue010@gmail.com>
2022-10-23 18:45:09 -0700

    How to print the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value
+ * @n: is the integer of the questiom
+ *
+ * Return: the abs of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
