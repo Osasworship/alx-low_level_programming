5d05211a99566952f58a733ca11e765517766469
Osasworship <amadasunosarugue010@gmail.com>
2022-10-25 06:38:40 -0700

    How to update the value a pointer points to as 98

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repository contains all the solutions to C pointers, stings and arrays task
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - Write the character C to stdout
+ * @c: The character to print
+ * Return: on success 1
+ * On eerror, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
