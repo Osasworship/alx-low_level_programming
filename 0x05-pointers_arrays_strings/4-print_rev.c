338c06d1b31c5d08a225e946459f266276d5b90f
Osasworship <amadasunosarugue010@gmail.com>
2022-10-25 13:32:43 -0700

    How to a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,16 +5,16 @@
  */
 void print_rev(char *s)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
+	while (i >= 0)
 	{
-		if (s[count] == '\0')
+		if (s[i] == '\0')
 			break;
-		count++;
+		i++;
 	}
 
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
