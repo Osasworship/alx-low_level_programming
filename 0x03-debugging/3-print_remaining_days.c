60950a3576a83883c584f107d9677dddab93c31d
Osasworship <amadasunosarugue010@gmail.com>
2022-10-26 20:35:44 -0700

    How to print day, month and year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -4,7 +4,8 @@
 /**
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
- * @month: month in number format
+ * @month:
+ * month: in number format
  * @day: day of month
  * @year: year
  * Return: void
@@ -12,7 +13,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 100 == 0) && (year % 100 == 0)))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -26,11 +27,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
+			printf("Invalid date: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
