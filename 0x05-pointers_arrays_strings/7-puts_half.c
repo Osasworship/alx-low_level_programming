e416ce8332de8eff24097ff7faf6a60159cfaf12
Osasworship <amadasunosarugue010@gmail.com>
2022-10-28 04:02:10 -0700

    How to print to print half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,20 +5,20 @@
  */
 void puts_half(char *str)
 {
-	int len = 0;
+	int len =  0, i;
 
-	while (*str != '\0')
+	while (len >= 0)
 	{
+		if (str[len] == '\0')
+			break;
 		len++;
-		str++;
 	}
+	if (len % 2 == 1)
+		i = len / 2;
+	else
+		i = (len - 1) / 2;
 
-	str == (len / 2);
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-
+	for (i++; i < len; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
