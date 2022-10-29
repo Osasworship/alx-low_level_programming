edb6f33b09235ffe8e59a0804d902083a8974eac
Osasworship <amadasunosarugue010@gmail.com>
2022-10-29 06:37:49 -0700

    How to generate valid password for 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,14 +3,14 @@
 #include <time.h>
 /**
  * main - generate keygen
- * Return: 0 AAlways
+ * Return: 0 Always
  */
 int main(void)
 {
-	int r = 0; c = 0;
-	time_h t;
+	int r = 0, c = 0;
+	time_t t;
 
-	srand((unsigned int) time (&t));
+	srand((unsigned int) time(&t));
 	while (c < 2772)
 	{
 		r = rand() % 128;
